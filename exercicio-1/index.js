625245e4c668c2fa98fce105fd25dc5315c122ad
gabriel soares <soaresgabriel155@gmail.com>
2024-09-11 14:59:57 -0300

    exercicio

--- exercicio-1/index.js
+++ exercicio-1/index.js
@@ -1,15 +1,15 @@
-const btnTorcarCor = document.getElementById("btn-trocar-cor")
+const btnTorcarCo = document.getElementById("btn-trocar-cor")
 
 const btnVoltarCor = document.getElementById("btn-voltar-cor")
 
 const quadradoAzul = document.querySelector(".azul")
 
-btnTorcarCor.addEventListener("click", function () {
+btnTorcarCo.addEventListener("click", function () {
     quadradoAzul.classList.remove("azul")
     quadradoAzul.classList.add("amarelo")
 })
 
-btnVoltarCor.addEventListener("click", function () {
+btnVoltarCo.addEventListener("click", function () {
     quadradoAzul.classList.remove("amarelo")
     quadradoAzul.classList.add("azul")
 })
