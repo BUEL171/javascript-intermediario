12058a99665f9fd32a3b73ac05693033c7a2170a
gabriel soares <soaresgabriel155@gmail.com>
2024-09-17 14:29:06 -0300

    exercicio

--- exercicio-6/src/js/index.js
+++ exercicio-6/src/js/index.js
@@ -1,7 +1,7 @@
 const itensPerguntasERespostas = document.querySelectorAll(".item")
 
 itensPerguntasERespostas.forEach(function i(item) {
-    item.addEventListener("click", function () {
+    item.addEventListener("click", function() {
         const itemAtivoAtual = document.querySelector(".ativo")
         if (itemAtivoAtual) {
             itemAtivoAtual.classList.remove("ativo")    
